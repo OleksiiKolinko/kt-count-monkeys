bd3218b2789ad0b62b91b61ae462f26dbeb24241
Oleksii Kolinko <138433107+OleksiiKolinko@users.noreply.github.com>
2025-06-22 15:31:32 +0200

    solution updated

--- src/main/kotlin/mate/academy/CountTheMonkeys.kt
+++ src/main/kotlin/mate/academy/CountTheMonkeys.kt
@@ -3,7 +3,6 @@ package mate.academy
 fun countTheMonkeys(count: Int): IntArray {
     if (count == 0) return intArrayOf()
     val intArray = IntArray(count)
-    for (i in 0 until count)
-        intArray[i] = i + 1
+    for (i in 0 until count) intArray[i] = i + 1
     return intArray
 }
